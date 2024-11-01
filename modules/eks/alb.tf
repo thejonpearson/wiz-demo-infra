3e00d728c384bbb8ffebd13b6f3150c0fe1bdd5d
Jon Pearson <7095401+thejonpearson@users.noreply.github.com>
2024-11-01 17:57:07 -0500

    add alb iam

---
+++ modules/eks/alb.tf
@@ -0,0 +1,18 @@
+
+# This module will create the IAM role and policy required for the AWS Load Balancer Controller to function.
+module "aws_load_balancer_controller_irsa_role" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.3.1"
+
+  role_name = "aws-load-balancer-controller"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
