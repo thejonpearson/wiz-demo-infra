7c50d04f262c9d42bcb38d9013c11079de2dd3f3
Jon Pearson <7095401+thejonpearson@users.noreply.github.com>
2024-11-03 14:06:32 -0600

    add s3 permissions, as well as admin per-reqs

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,6 @@
 # IAM Setup first
 
+# Create role for mongo instance to assume
 resource "aws_iam_role" "mongo-role" {
   name = "Mongo-Instance-Profile"
   assume_role_policy = jsonencode({
@@ -17,23 +18,48 @@ resource "aws_iam_role" "mongo-role" {
   })
 }
 
+# This is the appropriate "put objects in a dedicated s3 bucket" policy
+resource "aws_iam_role_policy" "mongo_s3_perms" {
+  name = "Mongo-S3-Permissions"
+  role = aws_iam_role.mongo-role.id
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Sid": "",
+        "Effect": "Allow",
+        "Action": [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:GetObjectAttributes",
+          "s3:GetObjectTagging",
+          "s3:DeleteObject",
+          "s3:GetObjectVersionAttributes"
+        ],
+        "Resource": "arn:aws:s3:::mongo*/*"
+      }
+    ]
+  })
+}
 
-# # This is the "granted Admin CSP permissions" requirement
-# # Listed seperately rather than replacing the SSM policy so we can do
-# # most testing without actually launching an outdated VM with admin access
-#
-# resource "aws_iam_role_policy_attachment" "aws_admin" {
-#   role       = aws_iam_role.role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-# }
-
-# # In production we'd use Secrets Manager or similar.
-# # For a PoC SSM is free, though the value is stored in plaintext in the statefile
+# # This lets us read SSM parameters
+# # Note: for production we'd use Secrets Manager or similar,
+# #   but SSM is free and works for this PoC. 
 #
 resource "aws_iam_role_policy_attachment" "ssm_access" {
   role       = aws_iam_role.mongo-role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess"
 }
+
+# This is the "granted Admin CSP permissions" requirement
+# Listed seperately rather than replacing the other policies so we can do
+# most testing without actually launching an outdated VM with admin access
+
+resource "aws_iam_role_policy_attachment" "aws_admin" {
+  role       = aws_iam_role.role.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
 
 resource "aws_iam_instance_profile" "mongo-profile" {
   name = "Mongo-Instance-Profile"
@@ -44,7 +70,7 @@ resource "aws_iam_instance_profile" "mongo-profile" {
 
 # # This was my initial though for creating user/pw, but it generates each 
 # # TF run, which effectively forces pw rotation every TF run. For POC, moving to
-# # using random_password 
+# # using random_string instead. 
 # data "aws_secretsmanager_random_password" "mongo-user" {
 #   password_length = 15
 #   include_space = false
@@ -69,6 +95,7 @@ resource "random_string" "mongo-pass" {
   special = false
 }
 
+# # NOTE: Even with type set to SecureString, the value is still stored in plaintext in the state file
 resource "aws_ssm_parameter" "mongo-user" {
   name  = "mongo-user"
   type  = "SecureString"
@@ -102,7 +129,6 @@ resource "aws_instance" "mongo-instance" {
 }
 
 # security group rules for allowing trafffic
-
 resource "aws_security_group" "mongo" {
   name   = "MongoDB"
   vpc_id = module.vpc.vpc_id
@@ -120,21 +146,33 @@ resource "aws_vpc_security_group_egress_rule" "allow_outbound" {
   }
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_eks" {
+  security_group_id            = aws_security_group.mongo.id
+  referenced_security_group_id = module.eks.node_security_group_id
+  ip_protocol                  = "tcp"
+  to_port                      = 27017
+  from_port                    = 27017
+  tags = {
+    Name = "Allow mongo connection from EKS"
+  }
+}
+
 # specifically assign elastic IP and allow ssh from me 
 
-resource "aws_eip" "testing-ip" {
-  instance = aws_instance.mongo-instance.id
-  domain   = "vpc"
-}
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.mongo.id
-  ip_protocol       = "tcp"
-  to_port           = 22
-  from_port         = 22
-  cidr_ipv4         = "24.217.129.67/32"
-}
+# resource "aws_eip" "testing-ip" {
+#   instance = aws_instance.mongo-instance.id
+#   domain   = "vpc"
+# }
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.mongo.id
+#   ip_protocol       = "tcp"
+#   to_port           = 22
+#   from_port         = 22
+#   cidr_ipv4         = "24.217.129.67/32"
+# }
 
-# dns for future tasky connection
+# "internal" dns
+# unregistered, so it won't resolve outside the VPC but that's all we want anyway
 
 resource "aws_route53_zone" "base" {
   name = "wizdemo.io"
